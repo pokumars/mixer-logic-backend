523f3363925eb0ff05f11dfe1c6389bae35eae2b
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-02-12 17:48:48 +0200

    make user schema and model for it

---
+++ database_models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, trim:true, minlength: 6, uniqueCaseInsensitive: true },
+  email: { type: String, required: true, trim:true, minlength: 6, uniqueCaseInsensitive: true },
+  passwordHash: { type: String, required: true },
+  favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Drink' }]
+});
+
+// when updating check this out https://www.npmjs.com/package/mongoose-unique-validator#find--updates
+userSchema.plugin(uniqueValidator);
+
+userSchema.set('toJSON',{
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject.passwordHash;
+    delete returnedObject._id;
+  },
+  versionKey: false
+});
+
+module.exports = mongoose.model('User', userSchema);
