36841284e7b29c7788df8c8bb8292ac325e7fe49
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-02-13 13:45:32 +0200

    add cross-env for different environment configuration

--- utility/config.js
+++ utility/config.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
+let MONGODB_URI = '';
+
+if (process.env.NODE_ENV === 'production') {
+  MONGODB_URI = process.env.MONGODB_URI;
+}
+
+if (process.env.NODE_ENV === 'development') {
+  MONGODB_URI = process.env.MONGODB_TRIAL_URI;
+}
+
+if (process.env.NODE_ENV === 'test') {
+  MONGODB_URI = process.env.MONGODB_TRIAL_URI;
+}
 
 const PORT = process.env.PORT;
 
