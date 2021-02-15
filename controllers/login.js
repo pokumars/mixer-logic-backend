156f442c8e09ad4c1bb398492b04afd9e0a7667f
Oheneba Poku-Marboah <ohenebakob@hotmail.com>
2021-02-15 05:33:39 +0200

    return token after successful ogin and error message if otherwise

--- controllers/login.js
+++ controllers/login.js
@@ -1,17 +1,42 @@
-const { request } = require('../app');
 const User = require('../database_models/user');
 const loginRouter = require('express').Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config= require('../utility/config');
 
 
-loginRouter.post('/', (request, response) => {
-  const body = request.body;
+loginRouter.post('/', async (request, response, next) => {
+  try {
+    const body = request.body;
 
-  // find user from db
+    // find user from db
+    const user = await User.findOne({ username: body.username });
+    if (!user) {//user does not exist
+      return response.status(401).send({ message: 'Access denied! There is no such user in our system' });
+    }
 
-  //compare passwordhash and decrypted password
+   //compare passwordhash and decrypted password
+    const passwordCorrect = await bcrypt.compare(body.password, user.passwordHash);
 
-  //send back token
+    //password is wrong
+    if (!passwordCorrect) {
+      return response.status(401).send({ message: ' The password and username does not match' });
+    }
 
+    //create token
+    const objToSign = {
+      username: user.username,
+      id: user.id
+    };
+    const token = jwt.sign(objToSign, config.TOKEN_SIGNATURE);
+
+    //send back token
+    return response.status(200).send({ token, user: user.toJSON() });
+
+  } catch (error) {
+    next(error);
+  }
 
 });
+
+module.exports = loginRouter;
